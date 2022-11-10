55a1f331544f0c785dca15d778f76b24913b3a29
Victor Eduardo <vcladara@gmail.com>
2022-11-10 02:36:00 -0300

    deixando responsivo parte-1

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,20 @@
-function organizeProjects () {
-    const projetos = document.querySelectorAll('.projeto')
-    const secao = document.querySelectorAll('.secao')
+function configMenu () {
+    const btn_menu = document.querySelector('.btn-menu')
+    const icone_btn = document.querySelector('.btn-menu .icone')
+    const menu = document.querySelector('header nav')
+    const fundo = document.createElement('div')
+    const body = document.querySelector('body')
+    
+    body.appendChild(fundo)
 
-    for (let i = 0; i < secao.length; i++) {
+    btn_menu.addEventListener('click', () => {
+        icone_btn.classList.toggle('on')
+        icone_btn.classList.toggle('off')
+        menu.classList.toggle('menu_mobile')
+        fundo.classList.toggle('fundo_blur')
+        body.classList.toggle('fixo')
 
-        secao[i].appendChild(projetos[i])
-        secao[i].appendChild(projetos[i + 1])
-
-        console.log(`${i} - ${i + 1}`)
-        console.log(secao[i])
-
-    }
+    })
 }
 
 function getProjects (api_propria) {
@@ -32,10 +36,10 @@ function getProjects (api_propria) {
                 if (i < 16) {
                     let descricao = e.description
                     let nome = e.name
-                    let screenshot = e.url
+                    let screenshot = null
                     let url = e.html_url
         
-                    screenshot = `https://raw.githubusercontent.com/${screenshot}/master/screenshots/screenshot.png`
+                    screenshot = `https://raw.githubusercontent.com/Victor-Eduardo-art/${nome}/master/screenshots/screenshot.png`
                     
                     const projeto = document.createElement('a')
                     const span = document.createElement('span')
@@ -44,41 +48,21 @@ function getProjects (api_propria) {
                     
                     projeto.href = url
                     span.innerHTML = descricao   
-                    // foto.src = screenshot
+                    foto.src = screenshot
                     foto.alt = 'screenshot do projeto'
 
                     ctr_foto.appendChild(foto)
 
                     projeto.classList.add('projeto')
                     ctr_foto.classList.add('screenshot')
+                    projeto.id = `projeto-${i}`
 
 
                     projeto.appendChild(span)
                     projeto.appendChild(ctr_foto)
                     root.appendChild(projeto)
-                    
-                    if (i%4 == 0) {
-                        const ctr_secoes = document.createElement('div')
-                        
-                        ctr_secoes.classList.add('ctr-secoes')
-                        ctr_secoes.id = Math.ceil(i/4 + 1)
-                        
-                        root.appendChild(ctr_secoes)
-
-                        const secao = document.createElement('div') // seção superior
-                        const secao2 = document.createElement('div') // seção inferior
-
-                        secao.classList.add('secao')
-                        secao2.classList.add('secao')
-
-                        ctr_secoes.appendChild(secao)
-                        ctr_secoes.appendChild(secao2)
-                    }
-
                 }
             })
-
-            organizeProjects()
         })
     } else {
         console.log('em criação..')
@@ -100,9 +84,12 @@ function configBotoes () {
 }
 
 function iniciar () {
-    if (location.href.indexOf('portfolio') != -1)
-    getProjects([false])
-    configBotoes()
+    configMenu()
+
+    if (location.href.indexOf('portfolio') != -1) {
+        getProjects([false])
+        configBotoes()
+    }
 
 }
 
